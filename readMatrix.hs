b08c69c89a58da463a3130b6ace522561aea4819
Patrycja <patrycja.cieplicka@gmail.com>
2020-12-28 20:55:33 +0100

    add filling with 4 on corner and 6 on boards" .

--- readMatrix.hs
+++ readMatrix.hs
@@ -17,11 +17,15 @@ readPuzzle filename = do
 
 main = do   puzzle <- readPuzzle "puzzle.txt"
             putStrLn (show $ puzzle)
+--            mozaic = toIntMatrix (removePuncMatrix puzzle)
+--            solution = createMatrix (numColumns mozaic) (numRows mozaic) 2
+--            filled_solution = fillAroundWhenVal mozaic solution (listOfCorners mozaic) 4
+
 
 
 -- implementacja macierzy
 type Matrix = [[Int]]
-type Pos = (Int,Int)
+type Pos = (Int,Int) -- iy/ix
 
 -- liczba wierszy macierzy
 numRows :: Matrix -> Int
@@ -73,6 +77,38 @@ extractValuesList s = map extractValues s
 toIntMatrix :: [String] -> [[Int]]
 toIntMatrix m = map extractValuesList m
 
+--sprawdzenie czy punkt istnieje
+ifExist :: Pos -> Matrix -> Bool 
+ifExist (iy,ix) m   | (iy < 0) || (ix < 0)  || ix > numColumns m || iy > numRows m = False 
+                    | otherwise = True
+
+--wygenerowanie listy rogów
+listOfCorners :: Matrix -> [Pos]
+listOfCorners m = [(iy,ix) | iy <- [0, numRows m - 1], ix <- [0, numColumns m - 1], ifExist (iy,ix) m]
+
+--wygenerowanie listy poziomych brzegow
+listOfHorizontalBorders :: Matrix -> [Pos]
+listOfHorizontalBorders m = [(iy,ix) | iy <-[0, numRows m - 1], ix <- [1..(numColumns m - 2)], ifExist (iy,ix) m]
+
+--wygenerowanie listy pionowych brzegow
+listOfVerticalBorders :: Matrix -> [Pos]
+listOfVerticalBorders m = [(iy,ix) | ix <-[0, numColumns m - 1], iy <- [1..(numRows m - 2)], ifExist (iy,ix) m]
+
+--wygenerowanie listy dziewięciu pozycji do odwiedzenia wokol podanej pozycji
+generateFillsPos :: Matrix -> Pos -> [Pos]
+generateFillsPos m (i,j) = [( i + a, j + b) | a <- [-1,0,1], b <- [-1,0,1], ifExist (i + a,j +b) m]
+
+--zapelnienie wartosci na podstawie podanej listy pozycji
+fillAround :: Matrix -> [Pos]-> Matrix
+fillAround solv [] = solv
+fillAround solv (x:xs)  = fillAround (updateElemMatrix x (\y -> 1) solv) xs
+
+--zapelnienie wartosci wokoł pozycji, ktore maja okreslona wartosc
+fillAroundWhenVal :: Matrix -> Matrix -> [Pos] -> Int -> Matrix
+fillAroundWhenVal _ solv [] _ = solv
+fillAroundWhenVal m solv (x:xs) val =   if elemMatrix m x == val 
+                                        then fillAroundWhenVal m (fillAround solv (generateFillsPos m x) ) xs val
+                                        else fillAroundWhenVal m solv xs val
 
 
 
