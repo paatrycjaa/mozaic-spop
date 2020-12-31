6f9e0ad14f21ca75dcb2d92b68d17fc7f8abe067
Patrycja <patrycja.cieplicka@gmail.com>
2020-12-31 16:26:28 +0100

    add filling obvious position - not working without function filling with 0 (to do - Marcin)

--- readMatrix.hs
+++ readMatrix.hs
@@ -41,7 +41,7 @@ createMatrix a b c = replicate b (replicate a c)
 
 --wydrukowanie wartosci macierzy m na pozycji (a, b) !! - indeksowanie maierzy
 elemMatrix :: Matrix -> Pos -> Int
-elemMatrix m (a, b) = m !! a !! b
+elemMatrix m (a, b) = m !! b !! a
 
 
 --zmienianie wartosci glowy listy
@@ -82,6 +82,11 @@ ifExist :: Pos -> Matrix -> Bool
 ifExist (iy,ix) m   | (iy < 0) || (ix < 0)  || ix > numColumns m || iy > numRows m = False 
                     | otherwise = True
 
+--sprawdzenie,czy pozycja wypelniona
+ifFilled :: Pos -> Matrix -> Bool 
+ifFilled (iy,ix) m  | elemMatrix m (iy,ix) /= 2 = True
+                    | otherwise = False 
+
 --wygenerowanie listy rogów
 listOfCorners :: Matrix -> [Pos]
 listOfCorners m = [(iy,ix) | iy <- [0, numRows m - 1], ix <- [0, numColumns m - 1], ifExist (iy,ix) m]
@@ -96,23 +101,53 @@ listOfVerticalBorders m = [(iy,ix) | ix <-[0, numColumns m - 1], iy <- [1..(numR
 
 --wygenerowanie listy elementów poza brzegami (9, ale i 8,7 nie mogą być na brzegu)
 listOfInsideElements :: Matrix -> [Pos]
-listOfInsideElements m = [(iy,ix) | ix <-[1..(numRows m - 2)], iy <- [1..(numRows m - 2)], ifExist (iy,ix) m]
+listOfInsideElements m = [(iy,ix) | ix <-[1..(numColumns m - 2)], iy <- [1..(numRows m - 2)], ifExist (iy,ix) m]
+
+--wygenerowanie listy wszytskich punktow
+listOfAllPositions :: Matrix -> [Pos]
+listOfAllPositions m = [(iy,ix) | ix <-[0..(numColumns m - 1)], iy <- [0..(numRows m - 1)], ifExist (iy,ix) m]
+
 
 --wygenerowanie listy dziewięciu pozycji do odwiedzenia wokol podanej pozycji
 generateFillsPos :: Matrix -> Pos -> [Pos]
 generateFillsPos m (i,j) = [( i + a, j + b) | a <- [-1,0,1], b <- [-1,0,1], ifExist (i + a,j +b) m]
 
+
 --zapelnienie wartosci na podstawie podanej listy pozycji
-fillAround :: Matrix -> [Pos]-> Matrix
-fillAround solv [] = solv
-fillAround solv (x:xs)  = fillAround (updateElemMatrix x (\y -> 1) solv) xs
+fillAround :: Matrix -> [Pos]-> Int -> Matrix
+fillAround solv [] _ = solv
+fillAround solv (x:xs) val = fillAround (updateElemMatrix x (\ y -> val) solv) xs --nie dziala dla zmiennej wartosci :(())
 
 --zapelnienie wartosci wokoł pozycji, ktore maja okreslona wartosc
-fillAroundWhenVal :: Matrix -> Matrix -> [Pos] -> Int -> Matrix
-fillAroundWhenVal _ solv [] _ = solv
-fillAroundWhenVal m solv (x:xs) val =   if elemMatrix m x == val 
-                                        then fillAroundWhenVal m (fillAround solv (generateFillsPos m x) ) xs val
-                                        else fillAroundWhenVal m solv xs val
+fillAroundWhenVal :: Matrix -> Matrix -> [Pos] -> Int -> Int -> Matrix
+fillAroundWhenVal _ solv [] _ _= solv
+fillAroundWhenVal m solv (x:xs) val fil=   if elemMatrix m x == val 
+                                        then fillAroundWhenVal m (fillAround solv (generateFillsPos m x) fil) xs val fil
+                                        else fillAroundWhenVal m solv xs val fil
 
+--zwrocenie liczby wypelnionych pozycji wokoł
+numberOfFilledPos :: Matrix -> Pos -> Int
+numberOfFilledPos m (iy,ix) = length [( iy + a, ix + b) | a <- [-1,0,1], b <- [-1,0,1], ifExist (iy + a,ix +b) m, ifFilled (iy + a,ix +b) m ]
 
+--zwrocenie liczby niewypelnionych pozycji wokoł
+numberNotOfFilledPos :: Matrix -> Pos -> Int
+numberNotOfFilledPos m (iy,ix) = length [( iy + a, ix + b) | a <- [-1,0,1], b <- [-1,0,1], ifExist (iy + a,ix +b) m, not (ifFilled (iy + a,ix +b) m )]
 
+--zwrocenie liczby wszystkich pozycji wokol
+numberOfAllPos :: Matrix -> Pos -> Int
+numberOfAllPos m (iy,ix) = length (generateFillsPos m (iy,ix))
+
+--wypelnienie pozostałych pozycji wokol wartoscia n
+fillAroundNotFilled :: Matrix -> Pos-> Int -> Matrix
+fillAroundNotFilled m (iy,ix) val = fillAround m  [( iy + a, ix + b) | a <- [-1,0,1], b <- [-1,0,1], ifExist (iy + a,ix +b) m, not (ifFilled (iy + a,ix +b) m )] val
+
+--wypełnienie oczywistych pozycji
+fillObviousPos :: Matrix -> Matrix -> [Pos]-> Matrix
+fillObviousPos _ solv [] = solv
+fillObviousPos m solv (x:xs) =    if ifFilled x solv == True 
+                                then fillObviousPos m solv xs
+                                else if elemMatrix m x == numberOfFilledPos solv x
+                                    then fillObviousPos m (fillAroundNotFilled solv x 0) xs
+                                    else if numberNotOfFilledPos solv x == (numberOfAllPos m x) - (elemMatrix m x)
+                                        then fillObviousPos m (fillAroundNotFilled solv x 1) xs
+                                        else fillObviousPos m solv xs
